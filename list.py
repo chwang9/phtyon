94d7cbfbb857c02c94619251e8efed48e290fd13
chwang9 <chwang9@iflytek.com>
2020-12-10 13:16:28 +0800

    list

---
+++ list.py
@@ -0,0 +1,37 @@
+#列表(列表可以存储混合类型)
+# nameList = ['xiaozhang', 'xiaowang', 'xiaoli']
+# print(nameList[0])
+
+# testList = ['xiaozhang', 0]
+# print(type(testList[1]))
+
+# nameList = ['xiaozhang', 'xiaowang', 'xiaoli']
+# for name in nameList:
+#     print(name)
+
+# 增
+# nameList = ['xiaozhang', 'xiaowang', 'xiaoli']
+# nameList.append(2)
+# print(nameList)
+
+# a = [1,2]
+# b = [3,4]
+# a.append(b)
+# print(a)
+# a.extend(b)
+# print(a)
+# a = [0,1,2]
+# a.insert(1,3)  # 第一个元素是下标，第二个元素是插入元素
+# print(a)
+
+#删
+# mouvie = ['HHH', 'www', 'wewewe']
+# del mouvie[1] # 删除指定位置的一个元素
+# mouvie.pop() # 删除最后一个
+# mouvie.remove('HHH') # 移除直接删除指定内容的元素
+# print(mouvie)
+
+#改
+# mouvie = ['HHH', 'www', 'wewewe']
+# mouvie[2] = '333'
+# print(mouvie)
